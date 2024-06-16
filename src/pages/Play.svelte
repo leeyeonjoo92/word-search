970eff3752c96595634de7cd51d5b48055ca27bf
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-16 18:44:21 +0900

    feat: play 페이지 마크업 및 css 적용
    - 게임 플레이 화면 마크업
    - 단어리스트, 퍼즐판, 현황판

--- src/pages/Play.svelte
+++ src/pages/Play.svelte
@@ -1,1 +1,47 @@
-<div>play</div>
+<form id="play-user-form">
+  <div class="play-user">
+    <label for="user" class="user-label">Name</label>
+    <input type="text" id="user" name="user" class="user-input" required />
+  </div>
+  <button type="submit" class="start-btn">Start</button>
+</form>
+
+<div class="puzzle-wrap">
+  <div class="puzzle-info-wrap">
+    <div class="puzzle-info">
+      <span>제목</span>
+      <span class="puzzle-timer">00:00</span>
+    </div>
+    <div class="puzzle-desc">설명</div>
+  </div>
+  <div class="puzzle-play">
+    <div class="puzzle-word-list">
+      {#each Array(10) as _, idx}
+        <span id="puzzle-word{idx}" class="puzzle-word">단어</span>
+      {/each}
+    </div>
+    <div class="puzzle-search">
+      {#each Array(168) as _, idx}
+        <span id="puzzle-alphabet{idx}" class="puzzle-alphabet">o</span>
+      {/each}
+    </div>
+    <div class="puzzle-status-board">
+      {#each Array(10) as _, idx}
+        <div class="puzzle-status-user">
+          <span id="puzzle-status-ranking{idx}" class="puzzle-status-ranking"
+            >{idx + 1}위</span
+          >
+          <span id="puzzle-status-name{idx}" class="puzzle-status-score"
+            >이름</span
+          >
+          <span id="puzzle-status-score{idx}" class="puzzle-status-score"
+            >점수</span
+          >
+          <span id="puzzle-status-time{idx}" class="puzzle-status-time"
+            >시간</span
+          >
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
