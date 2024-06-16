a885a7f87f3330f8a07f112b07eaf36293591ab5
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-16 13:49:27 +0900

    feat: maker 페이지 마크업 및 css 적용
    - 퍼즐 생성 & 퍼즐 업데이트 마크업 및 css 적용

--- src/pages/Maker.svelte
+++ src/pages/Maker.svelte
@@ -1,22 +1,57 @@
-<form>
-  <div class="maker-inputs">
-    <label for="title" class="maker-label">Title</label>
-    <input type="text" id="title" name="title" class="maker-input" required />
-  </div>
-  <div class="maker-inputs">
-    <label for="desc" class="maker-label">Description</label>
-    <input type="text" id="desc" name="desc" class="maker-input" required />
-  </div>
-  <div class="maker-word-list">
-    {#each Array(10) as _, idx}
+<script>
+  let clickSubmit = false;
+
+  const handleSubmit = () => {
+    clickSubmit = true;
+  };
+</script>
+
+{#if !clickSubmit}
+  <form id="maker-form">
+    <div class="maker-inputs">
+      <label for="title" class="maker-label">Title</label>
+      <input type="text" id="title" name="title" class="maker-input" required />
+    </div>
+    <div class="maker-inputs">
+      <label for="desc" class="maker-label">Description</label>
       <input
-        type="text"
-        id="word${idx}"
-        name="word${idx}"
-        class="maker-word"
+        type="textarea"
+        id="desc"
+        name="desc"
+        class="maker-input"
         required
       />
-    {/each}
+    </div>
+    <div class="maker-word-list-wrap">
+      <div class="maker-label">Word List</div>
+      <div class="maker-word-list">
+        {#each Array(10) as _, idx}
+          <input
+            type="text"
+            id="word${idx}"
+            name="word${idx}"
+            class="maker-input maker-word"
+            required
+          />
+        {/each}
+      </div>
+    </div>
+    <button type="submit" class="maker-submit-btn" on:click={handleSubmit}
+      >Submit</button
+    >
+  </form>
+{:else}
+<div class="maker-updated-wrap">
+  <div class="maker-updated-title">Puzzle Updated</div>
+  <div class="maker-updated">
+    <label for="maker-updated-link" class="maker-label">Play Link</label>
+    <input
+      type="text"
+      id="maker-updated-link"
+      name="maker-updated-link"
+      class="maker-input"
+      required
+    />
   </div>
-  <button type="submit" class="maker-submit-btn">Submit</button>
-</form>
+</div>
+{/if}
